ec86fdcbb28433b2ac58224f585134e2b19d2179
Jonathan Burkhart <91563311+jburkhart10@users.noreply.github.com>
2021-11-11 20:00:19 +0000

    sensor

--- student.py
+++ student.py
@@ -19,7 +19,7 @@ class Piggy(PiggyParent):
         '''
         self.LEFT_DEFAULT = 78
         self.RIGHT_DEFAULT = 80
-        self.MIDPOINT = 1500  # what servo command (1000-2000) is straight forward for your bot?
+        self.MIDPOINT = 1300  # what servo command (1000-2000) is straight forward for your bot?
         self.set_motor_power(self.MOTOR_LEFT + self.MOTOR_RIGHT, 0)
         self.load_defaults()
         
@@ -57,16 +57,15 @@ class Piggy(PiggyParent):
     ****************
     '''
     def jon(self):
-      print("starting square")
-      for i in range(4):
-        self.fwd()
-        time.sleep(3)
-        self.right(primary = 40, counter = -40)
-        time.sleep(1.2)
-        self.stop()
+      while True:
+        self.read_distance()
       
     def dance(self):
         """A higher-ordered algorithm to make your robot dance"""
+        
+        
+        
+        
         # TODO: check to see if it's safe before dancing
         
         # lower-ordered example...
